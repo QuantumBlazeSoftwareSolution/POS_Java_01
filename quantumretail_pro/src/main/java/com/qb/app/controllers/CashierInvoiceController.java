3faf5aa26948ecbc2d1b5c95d5f599426bcd4757
Vihanga Heshan Edirisingha <vihangaheshan37@gmail.com>
2025-03-04 22:07:23 +0530

    RePrintInvoice

--- quantumretail_pro/src/main/java/com/qb/app/controllers/CashierInvoiceController.java
+++ quantumretail_pro/src/main/java/com/qb/app/controllers/CashierInvoiceController.java
@@ -2,11 +2,15 @@ package com.qb.app.controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 public class CashierInvoiceController implements Initializable {
@@ -16,8 +20,22 @@ public class CashierInvoiceController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+
+        List<String> itemList = new ArrayList<>();
+        itemList.add("Tomato 1KG");
+        itemList.add("Chocolate 100g");
+        itemList.add("Chocolate 400g");
+        itemList.add("Coca Cola 1L");
+        itemList.add("Rice 5KG");
+        itemList.add("Coconut 1 Unit");
+        itemList.add("Milk Powder 400g");
+        itemList.add("Tea Leaves 200g");
+        itemList.add("Chilli Powder 250g Chilli Powder 250g Chilli Powder 250g");
+
         for (int i = 0; i < 10; i++) {
             try {
+                Random random = new Random();
+
                 // Load the invoiceItem.fxml file
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/qb/app/fxmlComponent/invoiceItem.fxml"));
                 Node invoiceItem = loader.load(); // Load the node
@@ -25,7 +43,7 @@ public class CashierInvoiceController implements Initializable {
                 InvoiceItemController itemController = loader.getController();
                 String itemCode = "ITEM #" + (i + 1);
                 String itemImage = getClass().getResource("/com/qb/app/assets/images/tomato.png").toExternalForm();
-                String itemName = "Product " + (i + 1);
+                String itemName = itemList.get(random.nextInt(itemList.size()));
                 double itemPrice = 100.00 * (i + 1);
                 double quantity = i + 1;
                 itemController.InvoiceItemData(itemCode, itemImage, itemName, itemPrice, quantity);
--- quantumretail_pro/src/main/java/com/qb/app/controllers/CashierRePrintController.java
+++ quantumretail_pro/src/main/java/com/qb/app/controllers/CashierRePrintController.java
@@ -1,13 +1,30 @@
 package com.qb.app.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.layout.VBox;
 
 public class CashierRePrintController implements Initializable {
 
+    @FXML
+    private VBox rePrintInvoiceItemContainer;
+    @FXML
+    private VBox rePrintItemContainer;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/qb/app/fxmlComponent/rePrintInvoiceItem.fxml"));
+            Node rePrintInvoiceItem = loader.load();
+
+            rePrintInvoiceItemContainer.getChildren().add(rePrintInvoiceItem);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
