6beafa44a4f349b09272c99c2e340b33764181d6
Vihanga Heshan Edirisingha <vihangaheshan37@gmail.com>
2025-03-26 05:35:58 +0530

    Design Product Registration Page

--- quantumretail_pro/src/main/java/com/qb/app/controllers/Product_registrationController.java
+++ quantumretail_pro/src/main/java/com/qb/app/controllers/Product_registrationController.java
@@ -1,23 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package com.qb.app.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
-/**
- * FXML Controller class
- *
- * @author Vihanga
- */
 public class Product_registrationController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    // <editor-fold desc="FXML init component" defaultstate="collapsed">
+    @FXML
+    private Group iconPage;
+    @FXML
+    private TextField tfItemName;
+    @FXML
+    private TextField tfBarCode;
+    @FXML
+    private ComboBox<?> cbBrand;
+    @FXML
+    private TextField tfSalePrice;
+    @FXML
+    private TextField tfCostPrice;
+    @FXML
+    private TextField tfDiscount;
+    @FXML
+    private ComboBox<?> cbUnit;
+    @FXML
+    private TextField tfMeasure;
+    @FXML
+    private ComboBox<?> cbType;
+    @FXML
+    private TextField tfParentID;
+    @FXML
+    private ImageView productImage;
+    @FXML
+    private Button btnPicture;
+    @FXML
+    private Button btnClear;
+    @FXML
+    private Button btnRegister;
+//    </editor-fold>
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
