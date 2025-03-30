b27096e41083581169fad9b83ec292bc5460b81f
Vihanga Heshan Edirisingha <vihangaheshan37@gmail.com>
2025-03-30 07:05:00 +0530

    Create Product Brand Management Interfacee

--- quantumretail_pro/src/main/java/com/qb/app/controllers/Product_brand_managementController.java
+++ quantumretail_pro/src/main/java/com/qb/app/controllers/Product_brand_managementController.java
@@ -1,26 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package com.qb.app.controllers;
 
+import com.qb.app.model.SVGIconGroup;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
 
-/**
- * FXML Controller class
- *
- * @author Vihanga
- */
 public class Product_brand_managementController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    //<editor-fold desc="FXML init component" defaultstate="collapsed">
+    @FXML
+    private Group iconPage;
+    //</editor-fold>
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        iconPage.getChildren().add(new SVGIconGroup("/com/qb/app/assets/icons/page-icon.svg"));
+    }
+
 }
