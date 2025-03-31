adfdca39e4a92fe31a8d7b7452b1cf67833ec018
Supun Sulakshana <supunsulakshana2001@gmail.com>
2025-03-31 17:42:55 +0530

    Update product overview complete design
    Co-Authored-By: Ravishka Indraji Ranaweera <138558029+ravi1280@users.noreply.github.com>

--- quantumretail_pro/src/main/java/com/qb/app/controllers/Product_overviewController.java
+++ quantumretail_pro/src/main/java/com/qb/app/controllers/Product_overviewController.java
@@ -6,7 +6,10 @@ package com.qb.app.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
 
 /**
  * FXML Controller class
@@ -15,12 +18,40 @@ import javafx.fxml.Initializable;
  */
 public class Product_overviewController implements Initializable {
 
+    @FXML
+    private BarChart<String, Number> productSalrChart;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        
+        loadChartData();
         // TODO
-    }    
+    } 
+    
+     private void loadChartData() {
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.setName("Product Sales");
+
+        // Add data to the series (Category, Value)
+        series.getData().add(new XYChart.Data<>("Product A", 50));
+        series.getData().add(new XYChart.Data<>("Product B", 80));
+        series.getData().add(new XYChart.Data<>("Product C", 30));
+        series.getData().add(new XYChart.Data<>("Product D", 100));
+        series.getData().add(new XYChart.Data<>("Product E", 70));
+        series.getData().add(new XYChart.Data<>("Product F", 80));
+        series.getData().add(new XYChart.Data<>("Product G", 40));
+        series.getData().add(new XYChart.Data<>("Product H", 90));
+        series.getData().add(new XYChart.Data<>("Product I", 50));
+        series.getData().add(new XYChart.Data<>("Product J", 80));
+        series.getData().add(new XYChart.Data<>("Product K", 60));
+        series.getData().add(new XYChart.Data<>("Product L", 100));
+
+        // Clear previous data and add the new series
+        productSalrChart.getData().clear();
+        productSalrChart.getData().add(series);
+    }
     
 }
