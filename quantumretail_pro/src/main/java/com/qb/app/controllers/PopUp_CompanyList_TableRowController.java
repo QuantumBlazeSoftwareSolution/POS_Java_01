2ceffbabf666416155eab7ace13a275463dea59a
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-04-02 14:02:49 +0530

    Create Pop Up Interface Table Row

---
+++ quantumretail_pro/src/main/java/com/qb/app/controllers/PopUp_CompanyList_TableRowController.java
@@ -0,0 +1,17 @@
+
+package com.qb.app.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class PopUp_CompanyList_TableRowController implements Initializable {
+
+  
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
