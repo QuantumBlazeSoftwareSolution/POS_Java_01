2d0b045343602c68f0784bd3c0a07cb88b2e9a7b
Vihanga Heshan Edirisingha <vihangaheshan37@gmail.com>
2025-05-15 20:31:46 +0530

    Update Cashier Session Controller

--- quantumretail_pro/src/main/java/com/qb/app/controllers/CashierSessionController.java
+++ quantumretail_pro/src/main/java/com/qb/app/controllers/CashierSessionController.java
@@ -21,7 +21,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.DialogPane;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.util.Duration;
