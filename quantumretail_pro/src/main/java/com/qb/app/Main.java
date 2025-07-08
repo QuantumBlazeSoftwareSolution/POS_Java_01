44eddba1943d81d9758d5ff52762860bc9eddd8c
Vihanga Heshan Edirisingha <vihangaheshan37@gmail.com>
2025-07-08 09:58:36 +0530

    Modify main class

--- quantumretail_pro/src/main/java/com/qb/app/Main.java
+++ quantumretail_pro/src/main/java/com/qb/app/Main.java
@@ -1,16 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.qb.app;
 
-/**
- *
- * @author Vihanga
- */
-public class Main {
+import com.qb.app.model.AppLogger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+
+    private static final Logger logger = AppLogger.getLogger();
+    
     public static void main(String[] args) {
         App.main(args);
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        Thread.currentThread().setUncaughtExceptionHandler((thread, throwable) -> {
+            logger.log(Level.SEVERE, "Uncaught Exception in Main Thread", throwable);
+        });
+
+        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
+            logger.log(Level.SEVERE, "Uncaught Exception in Thread: " + thread.getName(), throwable);
+        });
+
+        // Example scene
+        stage.setTitle("Demo App");
+        stage.setScene(new Scene(new Label("Hello")));
+        stage.show();
     }
 }
