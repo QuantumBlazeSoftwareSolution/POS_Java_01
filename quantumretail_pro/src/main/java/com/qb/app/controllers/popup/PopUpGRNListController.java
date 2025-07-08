a7c3b6029aadf87704a54df952a67e19456cb45f
Vihanga Heshan Edirisingha <vihangaheshan37@gmail.com>
2025-07-08 10:05:15 +0530

    Move popup controller classes & fxml files to popup package

---
+++ quantumretail_pro/src/main/java/com/qb/app/controllers/popup/PopUpGRNListController.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.qb.app.controllers.popup;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.Group;
+/**
+ * FXML Controller class
+ *
+ * @author Vihanga
+ */
+public class PopUpGRNListController implements Initializable {
+
+
+    @FXML
+    private Group pageIcon;
+    @FXML
+    private Group closeIcon;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
