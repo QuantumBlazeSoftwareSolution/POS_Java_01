57d22bde531fa2ab14d29c4bbe9c8710f22d8a99
ravi1280 <ravishkaindrajith9.9@gmail.com>
2026-01-13 13:02:21 +0530

    Create GRNListTable.java

---
+++ quantumretail_pro/src/main/java/com/qb/app/controllers/table_models/GRNListTable.java
@@ -0,0 +1,110 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.qb.app.controllers.table_models;
+
+import com.qb.app.model.entity.Product;
+import java.time.LocalDate;
+import java.util.Date;
+
+/**
+ *
+ * @author ravis
+ */
+public class GRNListTable {
+
+
+    // Create Variables 
+    private Product product;
+    private double qty;
+    private LocalDate expireDate;
+    private double costPrice;
+    private double salePrice;
+    private double discount;
+    private double amount;
+
+    // Create Constructor
+    public GRNListTable(
+            Product productData, double qtyData, LocalDate expireDateData, double costPriceData, double salePriceData, double discountData, double amountData) {
+
+        //Assign Vlaues 
+        this.product = productData;
+        this.qty = qtyData;
+        this.expireDate = expireDateData;
+        this.costPrice = costPriceData;
+        this.salePrice = salePriceData;
+        this.discount = discountData;
+        this.amount = amountData;
+
+    }
+    
+        public void recalculateAmount() {
+        double qtyVal = qty;
+        double costVal = costPrice;
+
+        double amount = qtyVal * costVal;
+            setAmount(amount);
+    }
+
+    
+    //Create Getter and Setters for  Accesss the data
+        public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public double getQty() {
+        return qty;
+    }
+
+    public void setQty(double qty) {
+        this.qty = qty;
+    }
+
+    public LocalDate getExpireDate() {
+        return expireDate;
+    }
+
+    public void setExpireDate(LocalDate expireDate) {
+        this.expireDate = expireDate;
+    }
+
+    public double getCostPrice() {
+        return costPrice;
+    }
+
+    public void setCostPrice(double costPrice) {
+        this.costPrice = costPrice;
+    }
+
+    public double getSalePrice() {
+        return salePrice;
+    }
+
+    public void setSalePrice(double salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+
+}
